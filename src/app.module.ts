bd665352b3b79be96a13087bb22dc5d576ad029d
Anderson Porto <anderson.higo2@gmail.com>
2024-04-10 11:12:06 -0300

    Add: conect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      host: process.env.POSTGRES_HOST,
+      database: 'postgres',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
